507bbb8a940d86afa5cc8b553c2b2d017adb0b06
garapatisaranya <h20240136@goa.bits-pilani.ac.in>
2026-01-23 13:18:37 +0530

    first commit

---
+++ jedi_omega_development_java_pos/src/com/flipfit/bean/Notification.java
@@ -0,0 +1,30 @@
+package com.flipkart.bean;
+
+public class Notification {
+	
+	private String notificationId;
+	private String message;
+	private String type;
+	
+	public String getNotificationId() {
+		return notificationId;
+	}
+	public void setNotificationId(String notificationId) {
+		this.notificationId = notificationId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+}
---
+++ jedi_omega_development_java_pos/src/com/flipfit/bean/Trainer.java
@@ -0,0 +1,35 @@
+package com.flipkart.bean;
+
+public class Trainer {
+	
+	private String trainerId;
+	private String name;
+	private String Specialization;
+	private String contact;
+	
+	public String getTrainerId() {
+		return trainerId;
+	}
+	public void setTrainerId(String trainerId) {
+		this.trainerId = trainerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecialization() {
+		return Specialization;
+	}
+	public void setSpecialization(String specialization) {
+		Specialization = specialization;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+}
