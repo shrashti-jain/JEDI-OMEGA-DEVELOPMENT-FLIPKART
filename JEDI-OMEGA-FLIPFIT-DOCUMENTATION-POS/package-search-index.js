87a64166a046ef30513cf82146748df10e7e326c
shreya8521 <shreyadokaniagrd@gmail.com>
2026-02-01 12:29:37 +0530

    Adding javadoc

---
+++ JEDI-OMEGA-FLIPFIT-DOCUMENTATION-POS/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.flipfit.bean"},{"l":"com.flipfit.business"},{"l":"com.flipfit.client"},{"l":"com.flipfit.constant"},{"l":"com.flipfit.dao"},{"l":"com.flipfit.exception"},{"l":"com.flipfit.utility"}];updateSearchResults();
